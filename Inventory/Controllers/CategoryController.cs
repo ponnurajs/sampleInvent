8015647bac1c6bf40f7d968279bb735f6a2b7b4d
ishokutty <ishokutty@DESKTOP-UN44JEP>
2017-02-09 21:04:57 -0500

    added category view

---
+++ Inventory/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Models;
+
+namespace Inventory.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var lstCategories = new List<Category>();
+            lstCategories.Add(new Category { Id = 1, Code = "CSMT", Name = "Cosmetic", Description = "Soap item" });
+            lstCategories.Add(new Category { Id = 2, Code = "MED", Name = "Medicine", Description = "Medical item" });
+
+            ViewBag.isButtonClick = true;
+
+            return View(lstCategories);
+        }
+    }
+}
